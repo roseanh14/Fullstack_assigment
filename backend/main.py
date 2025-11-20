084515732ba8204def471a6f240e14f25990ea72
roseanh <109952618+roseanh@users.noreply.github.com>
2025-11-20 12:57:18 +0100

    Update backend (routes/components), add tests, cleanup

--- backend/main.py
+++ backend/main.py
@@ -8,13 +8,10 @@ def create_app():
     app = Flask(__name__)
     CORS(app)
 
-    # registrace blueprintu s routami
     app.register_blueprint(api_bp)
 
     return app
 
-
-# exportovaná instance pro testy / produkci
 app = create_app()
 
 if __name__ == "__main__":
